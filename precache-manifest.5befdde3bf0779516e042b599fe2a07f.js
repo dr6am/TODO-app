e7eff906f3ce1b86076f2dd7a92f6d2d3017d607
Alex <alexanderbasoc@gmail.com>
2019-11-23 14:37:31 +0200

    Updates

---
+++ precache-manifest.5befdde3bf0779516e042b599fe2a07f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7542ae5b376c688d939b6566883ab759",
+    "url": "/TODO-app/index.html"
+  },
+  {
+    "revision": "5dd0c1b1510138d2da0b",
+    "url": "/TODO-app/static/css/main.8671eb0d.chunk.css"
+  },
+  {
+    "revision": "97725f03f602bd9ee384",
+    "url": "/TODO-app/static/js/2.475d8bbd.chunk.js"
+  },
+  {
+    "revision": "5dd0c1b1510138d2da0b",
+    "url": "/TODO-app/static/js/main.c0034131.chunk.js"
+  },
+  {
+    "revision": "794a71920b3b3e828a74",
+    "url": "/TODO-app/static/js/runtime-main.da742b33.js"
+  }
+]);
